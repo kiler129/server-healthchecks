9533f79e2470944866c5841e41834bccb87664eb
Grzegorz Zdanowski <grzegorz@noflash.pl>
2023-02-02 18:33:35 -0600

    Drop sh prefix

--- docker/http-middleware/docker-entrypoint.sh
+++ docker/http-middleware/docker-entrypoint.sh
@@ -4,9 +4,9 @@ cd /app
 
 if [[ "${UPDATE_ON_START-0}" -eq 1 ]]; then
   echo "Updating all scripts..."
-  /app/http-ping.sh -u
-  /app/with-healthcheck.sh -u
-  /app/http-middleware.sh -u
+  /app/http-ping -u
+  /app/with-healthcheck -u
+  /app/http-middleware -u
   echo "All updates finished, continuing startup"
 fi
 
