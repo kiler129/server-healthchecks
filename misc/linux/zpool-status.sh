7316cf7689dcfae4fcd581b606d56fee6ca7a736
Gregory Zdanowski-House <grzegorz@noflash.pl>
2023-06-12 21:00:43 -0500

    Change $status to $zstatus to avoid ZSH override
    When running under ZSH that is symlinked, $status shouldn't be overwritten, as it's a built-in r/o variable for exit code.

--- misc/linux/zpool-status.sh
+++ misc/linux/zpool-status.sh
@@ -14,12 +14,12 @@
 set -e -o errexit -o pipefail -o noclobber -o nounset
 
 # We abbreiviate status as the full one for larger pools will be horrendously long
-status=$(/sbin/zpool status "$@" | grep -E -e '^\s+?\w+:' | grep -v 'config:' | sed -r -e 's/^(\s+pool:)/\n\1/' | tail +2)
-echo "${status}"
+zstatus=$(/sbin/zpool status "$@" | grep -E -e '^\s+?\w+:' | grep -v 'config:' | sed -r -e 's/^(\s+pool:)/\n\1/' | tail +2)
+echo "${zstatus}"
 
 # Grep will exit with non-zero if there are no lines NOT having "ONLINE". If this script is
 # called without a pool name it will return exit=1 if at least one of the pools is not online
 poolsHealthy=0
-echo "${status}" | grep -E '^\s+state:\s+' | grep -v -E 'ONLINE$' > /dev/null 1>&2 || poolsHealthy=1
+echo "${zstatus}" | grep -E '^\s+state:\s+' | grep -v -E 'ONLINE$' > /dev/null 1>&2 || poolsHealthy=1
 
 if [[ $poolsHealthy -eq 0 ]]; then exit 1; fi
